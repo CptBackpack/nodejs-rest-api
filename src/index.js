500c34f2deacb350d8c2c56a3e64003213cfddad
CptBackpack <razvan.truta@toluna.com>
2019-10-29 15:21:22 +0200

    separate routes

--- src/index.js
+++ src/index.js
@@ -1,67 +1,39 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-
-let users = {
-    1: {
-        id: '1',
-        username: 'Robin Wieruch',
-    },
-    2: {
-        id: '2',
-        username: 'Dave Davids',
-    },
-};
-let messages = {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'Bye World',
-        userId: '2',
-    },
-};
+// import uuidv4 from 'uuid/v4';
+import routes from './routes';
+import models from './models/models';
 
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
+});
+
+app.use('/session', routes.session);
+app.use('/user', routes.user);
+app.use('/message', routes.message);
+
+
 
 app.get('/', (req, res) => {
-    return res.send(`Received a GET HTTP method`);
-});
-
-app.post('/', (req, res) => {
-    return res.send('Received a POST HTTP method');
-});
-
-app.put('/', (req, res) => {
-    return res.send('Received a PUT HTTP method');
-});
-
-app.delete('/', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
-});
-
-app.get('/users', (req, res) => {
-    return res.send(Object.values(users));
-});
-app.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
-});
-
-app.get('/messages', (req, res) => {
-    return res.send(Object.values(messages));
-});
-app.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
+  return res.send('Received a GET HTTP method');
 });
 
 
 
 app.listen(process.env.PORT, () =>
-    console.log('Example app listening on port ' + process.env.PORT),
+  console.log('Example app listening on port ' + process.env.PORT),
 );
---
+++ src/models/models.js
@@ -0,0 +1,28 @@
+
+let users = {
+    1: {
+        id: '1',
+        username: 'Cthulhu',
+    },
+    2: {
+        id: '2',
+        username: 'Nyarlathotep',
+    },
+};
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'Bye World',
+        userId: '2',
+    },
+};
+
+export default {
+    users,
+    messages,
+  };
