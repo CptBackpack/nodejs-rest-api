4eec6bb384f0f1928a083d40240762f0ddec50d2
CptBackpack <razvan.truta@toluna.com>
2019-10-29 14:49:25 +0200

    new setup

--- src/index.js
+++ src/index.js
@@ -1,104 +1,67 @@
-// ./src/index.js
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
 
-// importing the dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const {
-    startDatabase
-} = require('./database/mongo');
-const {
-    insertOp,
-    getOps,
-    deleteOp,
-    updateOp
-} = require('./database/ops');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
+let users = {
+    1: {
+        id: '1',
+        username: 'Robin Wieruch',
+    },
+    2: {
+        id: '2',
+        username: 'Dave Davids',
+    },
+};
+let messages = {
+    1: {
+        id: '1',
+        text: 'Hello World',
+        userId: '1',
+    },
+    2: {
+        id: '2',
+        text: 'Bye World',
+        userId: '2',
+    },
+};
 
-// defining the Express app
+
 const app = express();
 
-// defining an array to work as the database (temporary solution)
-
-// adding Helmet to enhance your API's security
-app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(bodyParser.json());
-
-
-// enabling CORS for all requests
 app.use(cors());
 
-// adding morgan to log HTTP requests
-app.use(morgan('combined'));
-
-// defining an endpoint to return all Items
-app.get('/', async (req, res) => {
-    res.send(await getOps());
+app.get('/', (req, res) => {
+    return res.send(`Received a GET HTTP method`);
 });
 
-const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: `https://dmbackpack.eu.auth0.com/.well-known/jwks.json`
-    }),
-  
-    // Validate the audience and the issuer.
-    audience: 'https://dmbackpack.eu.auth0.com/api/v2/',
-    issuer: `https://dmbackpack.eu.auth0.com/`,
-    algorithms: ['RS256']
-  });
-
-
-app.use(checkJwt);
-
-app.post('/', async (req, res) => {
-    const newOp = req.body;
-    await insertOp(newOp);
-    res.send({
-        message: req.body
-    });
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
 });
 
-// endpoint to delete an item
-app.delete('/:id', async (req, res) => {
-    await deleteOp(req.params.id);
-    res.send({
-        message: 'Item removed.'
-    });
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
 });
 
-// endpoint to update an item
-app.put('/:id', async (req, res) => {
-    const updatedOp = req.body;
-    await updateOp(req.params.id, updatedOp);
-    res.send({
-        message: req.body
-
-    });
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
 });
 
-// start the in-memory MongoDB instance
-startDatabase().then(async () => {
-    await insertOp({
-        title: "I AM INVALID",
-        value: "0000",
-        description: "1 N V 4 L 1 D"
-    });
-
-
-    // start the server
-    app.listen(3001, async () => {
-        console.log('listening on port 3001');
-    });
+app.get('/users', (req, res) => {
+    return res.send(Object.values(users));
 });
+app.get('/users/:userId', (req, res) => {
+    return res.send(users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+});
+app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId]);
+});
+
+
+
+app.listen(process.env.PORT, () =>
+    console.log('Example app listening on port ' + process.env.PORT),
+);
